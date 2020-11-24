4325da1a18b42fb8280fbcfacd9b6d989b3c28e3
Simon bertolf <simon.bertolf@reamis.ch>
2020-11-24 12:06:35 +0100

    Initial

--- server.php
+++ server.php
@@ -1,17 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',true);
+
+
 require_once('./app/system/user.php');
-echo 'sadg';
+
+echo '12';
 
 switch($_GET['action']) {
-			case 'login':
-				echo '12';
-				$login = new system_user();
-				echo 'df';
-				$login->login($_GET['name'], $_GET['pasword']);
-				break;
-			
-			default:
-				print_r('hallo du arsch');
-				break;
-		}
+	case 'login':
+		echo '12';
+		$login = new system_user();
+		echo 'df';
+		$login->login($_GET['name'], $_GET['pasword']);
+		break;
+	
+	case 'signin':
+		break;
+	
+	default:
+		print_r('hallo du arsch');
+		break;
+}
 	
