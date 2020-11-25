aae6123a262b9b55ec034d7801b4932389d8c2ad
Simon bertolf <simon.bertolf@reamis.ch>
2020-11-25 14:12:23 +0100

    Initial

--- server.php
+++ server.php
@@ -1,5 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:6006");
+header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: http://192.168.1.112:8080");
+
 require_once './app/UserDAO/UserDAOImp.php';
 require_once './app/DatabaseService/MSQLIProxi.php';
 require_once './app/Request/RequestImp.php';
@@ -8,3 +11,6 @@ $request = new RequestImp($_GET, $_POST);
 
 $databaseService = MSQLIProxi::getInstance();
 $userDAO = new UserDAOImp($databaseService);
+
+
+
