9aa87de444a3020e9fbefbf1f143f5fb0eae164a
Simon bertolf <simon.bertolf@reamis.ch>
2020-11-30 16:33:11 +0100

    Initial

---
+++ app/Controler/AddIncomeControler.php
@@ -0,0 +1,41 @@
+<?php
+require_once './app/Controler/Controler.php';
+
+class AddIncomeControler implements Controler {
+	
+	const ACTION = 'addIncome';
+	
+	//Wird später über die Session Abgefangen
+	private $userid = 1;
+	
+	/**
+	 * @var Controler
+	 */
+	private $nextControler;
+	/**
+	 * @var BudgetDAO
+	 */
+	private $budgetDAO;
+	
+	public function __construct($budgetDAO) {
+		$this->nextControler = null;
+		$this->budgetDAO = $budgetDAO;
+	}
+	
+	public function setNext($controler) {
+		$this->nextControler = $controler;
+	}
+	
+	/**
+	 * @param $request Request
+	 */
+	public function handle($request) {
+		if($request->get('action') === $this::ACTION){
+			$usage = $request['usage'];
+			$value = $request['value'];
+			return $this->budgetDAO->create($usage, $value, $this->userid);
+		}
+		if($this->nextControler) $this->nextControler->handle($request);
+	}
+	
+}
